dfbffb48949b178c4b4706b28e81597accc8789e
Tanaka <herog.vig@gmail.com>
2017-07-21 01:46:05 +0900

    initial commit.

---
+++ CustomerFeedback/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomerFeedback
+//
+//  Created by Dev on 7/21/17.
+//  Copyright © 2017 Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CustomerFeedback/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CustomerFeedback
+//
+//  Created by Dev on 7/21/17.
+//  Copyright © 2017 Dev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
